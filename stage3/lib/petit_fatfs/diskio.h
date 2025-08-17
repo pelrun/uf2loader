b34a702251b90349a3317b6d6ef6e877e68da22d
James Churchill <pelrun@gmail.com>
2025-08-17 17:30:13 +1000

    Replace stage3 sd card driver
    Now using ChaN's "Foolproof" SPI driver, ported by KenKen to pico

--- stage3/lib/petit_fatfs/diskio.h
+++ stage3/lib/petit_fatfs/diskio.h
@@ -29,11 +29,27 @@ typedef enum {
 /* Prototypes for disk control functions */
 
 DSTATUS disk_initialize (void);
-DRESULT disk_readp (BYTE* buff, DWORD sector, UINT offser, UINT count);
+DRESULT disk_readp (BYTE* buff, DWORD sector, UINT offset, UINT count);
 DRESULT disk_writep (const BYTE* buff, DWORD sc);
 
 #define STA_NOINIT		0x01	/* Drive not initialized */
 #define STA_NODISK		0x02	/* No medium in the drive */
+
+/* MMC card type flags (MMC_GET_TYPE) */
+#define CT_MMC3		0x01		/* MMC ver 3 */
+#define CT_MMC4		0x02		/* MMC ver 4+ */
+#define CT_MMC		0x03		/* MMC */
+#define CT_SDC1		0x04		/* SD ver 1 */
+#define CT_SDC2		0x08		/* SD ver 2+ */
+#define CT_SDC		0x0C		/* SD */
+#define CT_BLOCK	0x10		/* Block addressing */
+
+/* Generic command (Used by FatFs) */
+#define CTRL_SYNC			0	/* Complete pending write process (needed at _FS_READONLY == 0) */
+#define GET_SECTOR_COUNT	1	/* Get media size (needed at _USE_MKFS == 1) */
+#define GET_SECTOR_SIZE		2	/* Get sector size (needed at _MAX_SS != _MIN_SS) */
+#define GET_BLOCK_SIZE		3	/* Get erase block size (needed at _USE_MKFS == 1) */
+#define CTRL_TRIM			4	/* Inform device that the data on the block of sectors is no longer used (needed at _USE_TRIM == 1) */
 
 #ifdef __cplusplus
 }
